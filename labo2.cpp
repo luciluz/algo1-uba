aecaeb98c1afb907fa7c611f6bcc97cdba94567a
luciluz <104660670+luciluz@users.noreply.github.com>
2022-09-11 20:47:24 -0300

    Update labo2.cpp

--- labo2.cpp
+++ labo2.cpp
@@ -33,3 +33,41 @@ int main(){
 
 // Ejercicio 2
 
+# include <iostream>
+using namespace std;
+
+int cargarNota(int n) {
+    int nota;
+    cout << "ingresa nota " << n << ":" << endl;
+    cin >> nota;
+    if(nota == -1){
+        return nota;
+    }
+    while (nota < 0 || nota > 10) {
+        cout << "la nota no es válida, inténtalo de nuevo:" << endl;
+        cin >> nota;
+    }
+    return nota;
+}
+
+float promedio(float x, float y){
+    return x/y;
+}
+
+int main(){
+    int suma = 0;
+    int i = 1;
+    int a = 1;
+    int nota;
+    while (a != -1) {
+            nota = cargarNota(i);
+            if (nota == -1) {
+                a = nota;
+            } else {
+                suma += nota;
+            }
+            i++;
+        }
+    cout << "suma total: " << suma << endl;
+    cout << "promedio: " << promedio(suma, i-2) << endl;
+}
