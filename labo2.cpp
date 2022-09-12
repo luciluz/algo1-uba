5e30f562664ead56074a91ef0a68a9000b6a7cf3
luciluz <104660670+luciluz@users.noreply.github.com>
2022-09-11 23:04:24 -0300

    Update labo2.cpp

--- labo2.cpp
+++ labo2.cpp
@@ -120,3 +120,87 @@ return 0;
         cout << "aprobaron más de dos tercios, se puede incrementar el número de plazas del curso" << endl;
     }
 }
+
+// Ejercicio 4
+
+//Esta forma es para pasar de decimal a binario
+
+# include <iostream>
+using namespace std;
+
+int potencia(int x, int y){
+    int i = 1;
+    int p = x;
+    if (y == 0){
+        return 1;
+    } else {
+        while (i < y) {
+            x = x * p;
+            i++;
+        } return x;}
+}
+
+int pasarABinario(int n){
+    int m = n;
+    int binario = 0;
+    int k = 0;
+    while (m > 0){
+        if(m % 2 == 1){
+            binario += potencia(10,k);
+            m = (m-1)/2;
+            k++;
+        } else {
+           m = m/2;
+           k++;
+        }
+    }
+    return binario;
+}
+
+int main(){
+    int x;
+    cout << "Ingresa un número cualquiera: "<< endl;
+    cin >> x;
+    cout << "Tu número en binario es: " << pasarABinario(x) << endl;
+}
+
+// Esta manera es para pasar de binario a decimal
+
+# include <iostream>
+using namespace std;
+
+int potencia(int x, int y){
+    int i = 1;
+    int p = x;
+    if (y == 0){
+        return 1;
+    } else {
+        while (i < y) {
+            x = x * p;
+            i++;
+        } return x;}
+}
+
+int pasarADecimal(int n){
+    int m = n;
+    int decimal = 0;
+    int k = 0;
+    while (m > 0){
+        if(m % 10 != 0){
+            decimal += potencia(2,k);
+            m = (m-(m % 10))/10;
+            k++;
+        } else {
+           m = m/10;
+           k++;
+        }
+    }
+    return decimal;
+}
+
+int main(){
+    int x;
+    cout << "Ingresa un número en base binaria: "<< endl;
+    cin >> x;
+    cout << "Tu número en base decimal es: " << pasarADecimal(x) << endl;
+}
