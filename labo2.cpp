ae2ee95baa86c18f63b5ad0b2f7e12e8faf1f099
luciluz <104660670+luciluz@users.noreply.github.com>
2022-09-11 21:29:27 -0300

    Update labo2.cpp

--- labo2.cpp
+++ labo2.cpp
@@ -71,3 +71,52 @@ int main(){
     cout << "suma total: " << suma << endl;
     cout << "promedio: " << promedio(suma, i-2) << endl;
 }
+
+# include <iostream>
+using namespace std;
+
+// Ejercicio 3
+
+int cargarNota(int n) {
+    int nota;
+    cout << "ingresa nota " << n << ":" << endl;
+    cin >> nota;
+    if(nota == -1){
+        return nota;
+    }
+    while (nota < 0 || nota > 10) {
+        cout << "la nota no es válida, inténtalo de nuevo:" << endl;
+        cin >> nota;
+    }
+    return nota;
+}
+
+int main(){
+    int aprobaron = 0;
+    int total = 0;
+    int i = 1;
+    int centinela = 1;
+    int nota;
+    while (centinela != -1) {
+        nota = cargarNota(i);
+        if (nota == -1) {
+            centinela = nota;
+        } else {
+            if (nota > 6) {
+                aprobaron += 1;
+            }
+            i++;
+            total++;
+        }
+        }
+    if (total < 6) {
+cout << "Ojo piojo, la cantidad de alumnos no puede ser menor a 6. Reinicia el programa perejil" <<
+endl;
+return 0;
+}
+    cout << "aprobaron: " << aprobaron << endl;
+    cout << "reprobaron: " << i - aprobaron -1 << endl;
+    if (aprobaron > (2*(i-1))/3) {
+        cout << "aprobaron más de dos tercios, se puede incrementar el número de plazas del curso" << endl;
+    }
+}
